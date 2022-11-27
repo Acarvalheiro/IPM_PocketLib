a17d2f54d8bb77f9c38e0c0d1adf65328424a8a5
franriibeiro <fe.ribeiro@campus.fct.unl.pt>
2022-11-27 18:58:51 +0000

    selector with data from db

--- src/components/ReadListSelector.vue
+++ src/components/ReadListSelector.vue
@@ -1,21 +1,53 @@
 <template>
-    <ion-list>
-      <ion-item>
-        <ion-select interface="popover" placeholder="Your readbooks..">
-          <IonSelectOption value="favBooks">Favourite Books</IonSelectOption>
-          <IonSelectOption value="toRead">To read</IonSelectOption>
-          <IonSelectOption value="trending">Trending</IonSelectOption>
-        </ion-select>
-      </ion-item>
-    </ion-list>
-  </template>
+  <ion-list>
+    <ion-item>
+      <ion-select interface="popover" placeholder="Your readbooks..">
+        <IonSelectOption 
+        v-for="list in readlists" 
+        v-bind:key="list.id" 
+        :title="list.listName" 
+        :id="list.id"
+        >
+          <span @click="() => router.push('/profile')">
+            {{list.listName}}
+          </span>
+        </IonSelectOption>
+      </ion-select>
+    </ion-item>
+  </ion-list>
+</template>
   
-  <script>
-    import { IonList, IonItem, IonSelect, IonSelectOption } from '@ionic/vue';
-    import { defineComponent } from 'vue';
-  
-    export default defineComponent({
-    name: 'ReadListSelectorComp',
-      components: { IonList, IonItem, IonSelect, IonSelectOption },
-    });
-  </script>
+<script>
+import { IonList, IonItem, IonSelect, IonSelectOption } from '@ionic/vue';
+import { defineComponent } from 'vue';
+import { useFirestore, useCollection } from "vuefire";
+import { collection, addDoc, doc, deleteDoc } from "firebase/firestore";
+import { useRouter } from "vue-router";
+
+export default defineComponent({
+  name: 'ReadListSelectorComp',
+  components: {
+    IonList,
+    IonItem,
+    IonSelect,
+    IonSelectOption
+  },
+  data() {
+    return {
+      newList: "",
+      popoverOpen: false,
+    };
+  },
+  setup() {
+    const router = useRouter();
+    const db = useFirestore();
+    const listsRef = collection(db, "readlists");
+    const readlists = useCollection(collection(db, "readlists"));
+
+    return { router, readlists, listsRef, db };
+  },
+
+
+});
+
+</script>
